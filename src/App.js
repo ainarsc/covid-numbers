122af787816a7a4ec309a18fbdd9170a0d424e9f
Ainars Ciesa <ainars.c@gmail.com>
2020-10-22 21:27:23 -0600

    Add router and paths

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 // import logo from './logo.svg';
 import {Home} from './components/pages'
 import {Map} from './components/pages'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <Map />
+    <Router>
+      <Switch>
+        <Route path='/map' component={Map} />
+        <Route path='/' component={Home} />
+      </Switch>
+      <Map />
+    </Router>
+    
   );
 }
 
